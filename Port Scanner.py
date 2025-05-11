7ed4da8802872b67371ca008c61045e6e9d05ac9
MohamedSelimMah <mohamedselim.mahjoub@iteam-univ.tn>
2025-05-11 20:58:05 +0100

    commit

--- Port Scanner.py
+++ Port Scanner.py
@@ -1,13 +1,20 @@
 import asyncio
 import socket
-from datetime import datetime
 import ssl
-from typing import List, Tuple, Optional,Dict,Any
+from datetime import datetime
+from typing import List, Tuple, Optional, Dict, Any
 import argparse
 import logging
 import aiohttp
 import ipaddress
 import json
+import time
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, auto
 from collections import defaultdict
+
+default_timeout = 2.0
+default_concurrency = 512
+max_retries = 2
+Banner_grab_size= 1024
+
