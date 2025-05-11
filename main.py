6dbada4900477470f548321bcd22d3225760127f
MohamedSelimMah <mohamedselim.mahjoub@iteam-univ.tn>
2025-05-11 21:07:00 +0100

    commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from design.loading_screen import loading_screen
 from design.loading_screen import clear_screen
 from design.console import MyConsole
 from design.banner import show_random_banner
+from Script.Port_Scanner import *
 def main():
     loading_screen()
     clear_screen()
